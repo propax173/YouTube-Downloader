5ef9ae6e4188e72d4f6083b0040b37e89dd0dbd5
propax173 <80084217+propax173@users.noreply.github.com>
2023-08-02 12:39:20 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,20 @@
 import os
 
-class Config:
-    API_ID = os.getenv("23687011")
-    API_HASH = os.getenv("ecb7e7889511fbb8e754d829a4f8fcfe")
-    BOT_TOKEN = os.getenv("6680464792:AAFrULplB7SP9jBwDcV0ze-KSlb7-_Z-dk4")
+class Config(object):
+
+    # get a token from @BotFather
+    BOT_TOKEN = "6680464792:AAFrULplB7SP9jBwDcV0ze-KSlb7-_Z-dk4"
+
+    # Get these values from my.telegram.org
+    API_ID = 23687011
+    API_HASH = "ecb7e7889511fbb8e754d829a4f8fcfe"
+
+    # No need to change
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    ADL_BOT_RQ = {}
+    CHUNK_SIZE = 128
+    TG_MAX_FILE_SIZE = 4194304000
+    HTTP_PROXY = ""
+    PROCESS_MAX_TIMEOUT = 3700
+    # auth users
+    AUTH_USERS = [911285605]
