8e448b26454ef7662125ee4b7448fe7b8a409ccb
propax173 <80084217+propax173@users.noreply.github.com>
2023-08-02 12:37:09 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("23687011"))
+    API_ID = os.getenv("23687011")
     API_HASH = os.getenv("ecb7e7889511fbb8e754d829a4f8fcfe")
     BOT_TOKEN = os.getenv("6680464792:AAFrULplB7SP9jBwDcV0ze-KSlb7-_Z-dk4")
